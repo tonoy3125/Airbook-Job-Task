4c49497b0b4d1127f70631de6e8946330bd3eac3
tonoy3125 <shaifshajedt@gmail.com>
2024-03-19 16:33:51 +0600

    search button added and some padding and margin added

--- src/Components/Banner Component/BannerComponent.jsx
+++ src/Components/Banner Component/BannerComponent.jsx
@@ -1,4 +1,5 @@
 import { IoIosArrowDown } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
 
 const BannerComponent = () => {
     return (
@@ -26,33 +27,38 @@ const BannerComponent = () => {
                 </div>
             </div>
             <div className="flex items-center gap-5">
-                <div className="mt-5 border flex items-center px-5 py-2 rounded-lg ">
+                <div className="mt-5 border flex items-center px-10 py-2 rounded-lg ">
                     <h1 className="text-[#1E1E1E] text-sm font-semibold border-r border-r-[#09182E]  pr-3">DAC</h1>
                     <div className="pl-3">
                         <h3 className="text-sm font-semibold text-[#1E1E1E]">Dhaka</h3>
                         <h5 className="text-[10px] ">Hazrat Shahjalal International</h5>
                     </div>
                 </div>
-                <div className="mt-5 border flex items-center px-5 py-2 rounded-lg">
+                <div className="mt-5 border flex items-center px-10 py-2 rounded-lg">
                     <h1 className="text-[#1E1E1E] text-sm font-semibold border-r border-r-[#09182E]  pr-3">Cxb</h1>
                     <div className="pl-3">
                         <h3 className="text-sm font-semibold text-[#1E1E1E]">Cox’s Bazar</h3>
                         <h5 className="text-[10px]">Cox’s Bazar international</h5>
                     </div>
                 </div>
-                <div className="mt-5 border flex items-center px-5 py-2 rounded-lg">
+                <div className="mt-5 border flex items-center px-10 py-2 rounded-lg">
                     <h1 className="text-[#1E1E1E] text-sm font-semibold border-r border-r-[#09182E]  pr-3">03</h1>
                     <div className="pl-3">
                         <h3 className="text-sm font-semibold text-[#1E1E1E]">October</h3>
                         <h5 className="text-[10px]">Tuesday, 2023</h5>
                     </div>
                 </div>
-                <div className="mt-5 border flex items-center px-5 py-2 rounded-lg">
+                <div className="mt-5 border flex items-center px-10 py-2 rounded-lg">
                     <h1 className="text-[#1E1E1E] text-sm font-semibold border-r border-r-[#09182E]  pr-3">05</h1>
                     <div className="pl-3">
                         <h3 className="text-sm font-semibold text-[#1E1E1E]">October</h3>
                         <h5 className="text-[10px]">Tuesday, 2023</h5>
                     </div>
+                </div>
+                <div className="mt-5 border flex items-center px-5 py-[14px] rounded-lg bg-[#FFB700]">
+                    <button>
+                        <FaSearch className="text-2xl text-white" />
+                    </button>
                 </div>
             </div>
         </div>
